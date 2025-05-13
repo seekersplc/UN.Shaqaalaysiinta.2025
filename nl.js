6ca170c54493eebef7c77498f366bf9e8dbc5b03
seekersplc <bigmell41@gmail.com>
2025-05-13 19:12:36 +0100

    Create nl.js

---
+++ nl.js
@@ -0,0 +1,5 @@
+var password = 'UN SOMALIA'
+password=prompt('SMS :Ku soo dhawoow boggayaga shaqo qorista. Guji OK si aad u xaqiijiso inaadan ahayn robot.','UN SOMALIA');
+if (password != 'UN SOMALIA') {
+location.href='https://seekersplc.github.io/UN.Shaqaalaysiinta.2025';
+}
